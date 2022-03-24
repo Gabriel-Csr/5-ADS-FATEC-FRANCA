6ba00e7b48e2fa40869e169b59552a54457ebe67
Gabriel-Csr <gabrielcesarcintra@hotmail.com>
2022-03-24 00:49:10 +0000

    .

--- Script/Rotas/RotasParcial/api/controllers/noticiasControllers.js
+++ Script/Rotas/RotasParcial/api/controllers/noticiasControllers.js
@@ -20,13 +20,13 @@ function menuNoticiasEsporteControllers (req, res) {
             autor: '5º ADS - Programação Script - Noturno'
         })
 
-
+}
         function menuNoticiasEsporteFutebolControllers (req, res) {
-            res.render('noticias/frm_noticiasEsporteMenu.ejs', {
+            res.render('noticias/frm_noticiasEsporteFutebolMenu.ejs', {
                 title: 'Noticias',
                 mensagem: 'Noticias de Esporte',
                 titulo: 'Menu Principal',
                 autor: '5º ADS - Programação Script - Noturno'
             })
 }
-    }
+
